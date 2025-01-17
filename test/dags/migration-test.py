35e2b91fa8061acfbd4bcd870cd0d7bf289e13e6
AdrianCubilloGorilla <adrian.cubillo@gorillalogic.com>
2025-01-17 16:44:09 -0600

    testing

--- test/dags/migration-test.py
+++ test/dags/migration-test.py
@@ -29,7 +29,7 @@ with DAG(
         schema='zuora',
         table='product_rate_plan_charge_tiers_v1',
         partition_name='snapshot_day={{ ds }}', 
-        metastore_conn_id='hive_metastore',
+        mysql_conn_id='hive_metastore',
         poke_interval=60,
         timeout=600
     )
